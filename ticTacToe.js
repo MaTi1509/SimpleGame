b6c71861d86c39d36aa31c98ff37b0f3d3d734d4
Mateusz Lizurek <92801560+MaTi1509@users.noreply.github.com>
2023-08-14 16:40:01 +0200

    Poprawienie linii

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,30 +1,21 @@
 const item1Symbol = document.querySelector('#item1Symbol');
 const item1Button = document.querySelector('#item1');
-
 const item2Symbol = document.querySelector('#item2Symbol');
 const item2Button = document.querySelector('#item2');
-
 const item3Symbol = document.querySelector('#item3Symbol');
 const item3Button = document.querySelector('#item3');
-
 const item4Symbol = document.querySelector('#item4Symbol');
 const item4Button = document.querySelector('#item4');
-
 const item5Symbol = document.querySelector('#item5Symbol');
 const item5Button = document.querySelector('#item5');
-
 const item6Symbol = document.querySelector('#item6Symbol');
 const item6Button = document.querySelector('#item6');
-
 const item7Symbol = document.querySelector('#item7Symbol');
 const item7Button = document.querySelector('#item7');
-
 const item8Symbol = document.querySelector('#item8Symbol');
 const item8Button = document.querySelector('#item8');
-
 const item9Symbol = document.querySelector('#item9Symbol');
 const item9Button = document.querySelector('#item9');
-
 const player2 = document.querySelector('.player2');
 const player1 = document.querySelector('.player1');
 let counter1 = 0;
