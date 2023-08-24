de25b32347da2365b46f28d94f03f0e70da0bb68
MaTi1509 <l.mati1509@gmail.com>
2023-08-24 14:49:09 +0200

    dodanie zman w JS

--- ticTacToe.js
+++ ticTacToe.js
@@ -123,8 +123,8 @@ function closeGameInfo() {
 function closeWinInfo() {
   resetSymbol();
   moveCounter = 0;
-  isWin = false;
   winInfo.style.display = "none";
+  isWin = false;
 }
 
 setSymbol(item1Button, item1Symbol);
@@ -136,4 +136,3 @@ setSymbol(item6Button, item6Symbol);
 setSymbol(item7Button, item7Symbol);
 setSymbol(item8Button, item8Symbol);
 setSymbol(item9Button, item9Symbol);
-resetSymbols();
